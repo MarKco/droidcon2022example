3de81f96fda8c5153011e79fbf782bc804f0355f
Marco Zanetti <ext.marco.zanetti@deltatre.com>
2022-09-21 00:26:27 +0200

    Added Tile manager and explicit adding of custom tile with Quick Settings Placement API

--- app/src/main/java/com/example/droidcondemoapp/MainActivity.kt
+++ app/src/main/java/com/example/droidcondemoapp/MainActivity.kt
@@ -1,13 +1,19 @@
 package com.example.droidcondemoapp
 
+import android.annotation.SuppressLint
 import android.app.Activity
+import android.app.StatusBarManager
+import android.content.ComponentName
+import android.content.Context
 import android.content.Intent
+import android.graphics.drawable.Icon
 import android.net.Uri
 import android.os.Build
 import android.os.Bundle
 import android.os.ext.SdkExtensions.getExtensionVersion
 import android.provider.MediaStore
 import android.util.Log
+import android.widget.Button
 import android.widget.ImageView
 import android.widget.Toast
 import androidx.activity.result.PickVisualMediaRequest
@@ -30,14 +36,40 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
+    @SuppressLint("WrongConstant")
     @RequiresApi(Build.VERSION_CODES.TIRAMISU)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        startPhotoPickerOrWhatev()
 
         findViewById<ConstraintLayout>(R.id.background_layout).setOnClickListener {
             startPhotoPickerOrWhatev()
+        }
+
+        findViewById<Button>(R.id.button_for_tile).setOnClickListener {
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+                val statusBarManager =
+                    getSystemService(Context.STATUS_BAR_SERVICE) as StatusBarManager
+                /**
+                 * Important: Change the package name accordingly to your own package name!
+                 */
+                statusBarManager.requestAddTileService(
+                    ComponentName(
+                        "com.example.droidcondemoapp",
+                        "com.example.droidcondemoapp.MySampleTileService",
+                    ),
+                    "Droidcon Example App",
+                    Icon.createWithResource(this, R.drawable.ic_launcher_foreground),
+                    {},
+                    {}
+                )
+            } else {
+                Toast.makeText(
+                    this,
+                    "`requestAddTileService` can only be called in Android 13/Tiramisu.",
+                    Toast.LENGTH_SHORT,
+                ).show()
+            }
         }
     }
 
