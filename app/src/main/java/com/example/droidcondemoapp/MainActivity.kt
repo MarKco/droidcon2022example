1ffa048db2346855ea030e7d02721f43e6a3217e
Marco Zanetti <ext.marco.zanetti@deltatre.com>
2022-09-29 23:33:30 +0200

    Added easy example of per-app language

--- app/src/main/java/com/example/droidcondemoapp/MainActivity.kt
+++ app/src/main/java/com/example/droidcondemoapp/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.droidcondemoapp
 import android.Manifest
 import android.annotation.SuppressLint
 import android.app.Activity
+import android.app.LocaleManager
 import android.app.StatusBarManager
 import android.content.ComponentName
 import android.content.Context
@@ -11,6 +12,7 @@ import android.graphics.drawable.Icon
 import android.net.Uri
 import android.os.Build
 import android.os.Bundle
+import android.os.LocaleList
 import android.os.ext.SdkExtensions.getExtensionVersion
 import android.provider.MediaStore
 import android.util.AttributeSet
@@ -18,15 +20,19 @@ import android.util.Log
 import android.view.View
 import android.widget.Button
 import android.widget.ImageView
+import android.widget.TextView
 import android.widget.Toast
 import androidx.activity.result.PickVisualMediaRequest
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.annotation.RequiresApi
 import androidx.appcompat.app.AppCompatActivity
 import androidx.constraintlayout.widget.ConstraintLayout
+import java.util.*
 
 
 class MainActivity : AppCompatActivity() {
+
+    private var localeManager: LocaleManager? = null
 
     // Registers a photo picker activity launcher in single-select mode.
     val pickMedia = registerForActivityResult(ActivityResultContracts.PickVisualMedia()) { uri ->
@@ -79,6 +85,36 @@ class MainActivity : AppCompatActivity() {
                     Toast.LENGTH_SHORT,
                 ).show()
             }
+        }
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+            localeManager =
+                getSystemService(Context.LOCALE_SERVICE) as LocaleManager
+        }
+
+        findViewById<Button>(R.id.button_lang_en).setOnClickListener {
+            localeManager?.applicationLocales = LocaleList(Locale.forLanguageTag("en"))
+        }
+        findViewById<Button>(R.id.button_lang_it).setOnClickListener {
+            localeManager?.applicationLocales = LocaleList(Locale.forLanguageTag("it"))
+        }
+        findViewById<Button>(R.id.button_lang_reset).setOnClickListener {
+            localeManager?.applicationLocales = LocaleList.getEmptyLocaleList()
+        }
+
+    }
+
+    override fun onResume() {
+        super.onResume()
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+            val language = when (localeManager?.applicationLocales?.toLanguageTags()) {
+                "en" -> "English"
+                "it" -> "Italian"
+                else -> "Not Set"
+            }
+            Log.wtf("LANG", localeManager?.applicationLocales?.toLanguageTags())
+            findViewById<TextView>(R.id.language_textview).text =
+                "Current In-App Language: $language"
         }
     }
 
