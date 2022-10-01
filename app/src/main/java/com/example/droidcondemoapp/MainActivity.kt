137634b31d6e3ef2e179afa2eafecb179dbf13dd
Marco Zanetti <ext.marco.zanetti@deltatre.com>
2022-10-01 07:26:05 +0200

    Testing wifi discovery permissions on Android 13 and lower

--- app/src/main/java/com/example/droidcondemoapp/MainActivity.kt
+++ app/src/main/java/com/example/droidcondemoapp/MainActivity.kt
@@ -1,13 +1,25 @@
 package com.example.droidcondemoapp
 
 import android.annotation.SuppressLint
+import android.content.Context
+import android.content.IntentFilter
+import android.net.wifi.WifiManager
+import android.net.wifi.p2p.WifiP2pManager
 import android.os.Build
 import android.os.Bundle
+import android.util.Log
+import android.widget.Button
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.annotation.RequiresApi
 import androidx.appcompat.app.AppCompatActivity
 
 
 class MainActivity : AppCompatActivity() {
+
+    private val intentFilter = IntentFilter()
+    private lateinit var channel: WifiP2pManager.Channel
+    private lateinit var manager: WifiP2pManager
+    private lateinit var receiver: WifiReceiver
 
     @SuppressLint("WrongConstant")
     @RequiresApi(Build.VERSION_CODES.TIRAMISU)
@@ -15,6 +27,75 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        // Indicates a change in the Wi-Fi Direct status.
+        intentFilter.addAction(WifiP2pManager.WIFI_P2P_STATE_CHANGED_ACTION)
+
+        // Indicates a change in the list of available peers.
+        intentFilter.addAction(WifiP2pManager.WIFI_P2P_PEERS_CHANGED_ACTION)
+
+        // Indicates the state of Wi-Fi Direct connectivity has changed.
+        intentFilter.addAction(WifiP2pManager.WIFI_P2P_CONNECTION_CHANGED_ACTION)
+
+        // Indicates this device's details have changed.
+        intentFilter.addAction(WifiP2pManager.WIFI_P2P_THIS_DEVICE_CHANGED_ACTION)
+
+        manager = getSystemService(Context.WIFI_P2P_SERVICE) as WifiP2pManager
+        channel = manager.initialize(this, mainLooper, null)
+
+        val locationPermissionRequest = registerForActivityResult(
+            ActivityResultContracts.RequestMultiplePermissions()
+        ) { permissions ->
+            when {
+                permissions.getOrDefault("android.permission.ACCESS_FINE_LOCATION", false) -> {
+                    // Precise location access granted.
+                }
+                permissions.getOrDefault("android.permission.ACCESS_COARSE_LOCATION", false) -> {
+                    // Only approximate location access granted.
+                }
+                else -> {
+                    // No location access granted.
+                }
+            }
+        }
+
+        locationPermissionRequest.launch(
+            arrayOf(
+                "android.permission.ACCESS_FINE_LOCATION",
+                "android.permission.ACCESS_COARSE_LOCATION"
+            )
+        )
+
+        val wifiService = applicationContext.getSystemService(Context.WIFI_SERVICE) as WifiManager
+
+        findViewById<Button>(R.id.button_for_tile).setOnClickListener {
+            wifiService.startScan()
+            manager.discoverPeers(channel, object : WifiP2pManager.ActionListener {
+
+                override fun onSuccess() {
+                    // Code for when the discovery initiation is successful goes here.
+                    // No services have actually been discovered yet, so this method
+                    // can often be left blank. Code for peer discovery goes in the
+                    // onReceive method, detailed below.
+                }
+
+                override fun onFailure(reasonCode: Int) {
+                    Log.d("DroidconDemoApp", "No devices found")
+                    // Code for when the discovery initiation fails goes here.
+                    // Alert the user that something went wrong.
+                }
+            })
+        }
+    }
+
+    override fun onResume() {
+        super.onResume()
+        receiver = WifiReceiver(manager, channel, this)
+        registerReceiver(receiver, intentFilter)
+    }
+
+    override fun onPause() {
+        super.onPause()
+        unregisterReceiver(receiver)
     }
 
 }
---
+++ app/src/main/java/com/example/droidcondemoapp/WifiReceiver.kt
@@ -0,0 +1,35 @@
+package com.example.droidcondemoapp
+
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+import android.net.wifi.p2p.WifiP2pManager
+
+class WifiReceiver(manager: WifiP2pManager, channel: WifiP2pManager.Channel, mainActivity: MainActivity) : BroadcastReceiver() {
+
+    override fun onReceive(context: Context, intent: Intent) {
+        when (intent.action) {
+            WifiP2pManager.WIFI_P2P_STATE_CHANGED_ACTION -> {
+                // Determine if Wi-Fi Direct mode is enabled or not, alert
+                // the Activity.
+                val state = intent.getIntExtra(WifiP2pManager.EXTRA_WIFI_STATE, -1)
+            }
+            WifiP2pManager.WIFI_P2P_PEERS_CHANGED_ACTION -> {
+
+                // The peer list has changed! We should probably do something about
+                // that.
+
+            }
+            WifiP2pManager.WIFI_P2P_CONNECTION_CHANGED_ACTION -> {
+
+                // Connection state changed! We should probably do something about
+                // that.
+
+            }
+            WifiP2pManager.WIFI_P2P_THIS_DEVICE_CHANGED_ACTION -> {
+
+            }
+        }
+    }
+
+}
