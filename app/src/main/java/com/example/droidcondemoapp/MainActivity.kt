71f07c82f8126fb9d55b4ca390614dd518540a2b
Marco Zanetti <ext.marco.zanetti@deltatre.com>
2022-10-01 10:00:23 +0200

    Added (fake) dialog for downgraded permissions

--- app/src/main/java/com/example/droidcondemoapp/MainActivity.kt
+++ app/src/main/java/com/example/droidcondemoapp/MainActivity.kt
@@ -4,6 +4,7 @@ import android.annotation.SuppressLint
 import android.os.Build
 import android.os.Bundle
 import androidx.annotation.RequiresApi
+import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
 
 
@@ -14,6 +15,14 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        AlertDialog.Builder(this)
+            .setTitle("Permission revoked")
+            .setPositiveButton("OK") { dialog, which ->
+
+            }
+            .setMessage("The latest app version doesn't longer need to check for user's location, thus the matching permission was automatically self-revoked. We care about your privacy :-) ")
+            .show()
 
     }
 
