d80681486aa3c6f679f1829a0c960c61a4a99058
Marco Zanetti <ext.marco.zanetti@deltatre.com>
2022-10-01 06:54:54 +0200

    Removed custom tiles - moved to its branch

--- app/src/main/java/com/example/droidcondemoapp/MainActivity.kt
+++ app/src/main/java/com/example/droidcondemoapp/MainActivity.kt
@@ -1,18 +1,8 @@
 package com.example.droidcondemoapp
 
 import android.annotation.SuppressLint
-import android.app.Activity
-import android.app.StatusBarManager
-import android.content.ComponentName
-import android.content.Context
-import android.content.Intent
-import android.graphics.drawable.Icon
-import android.net.Uri
 import android.os.Build
 import android.os.Bundle
-import android.widget.Button
-import android.widget.ImageView
-import android.widget.Toast
 import androidx.annotation.RequiresApi
 import androidx.appcompat.app.AppCompatActivity
 
@@ -25,44 +15,6 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        findViewById<Button>(R.id.button_for_tile).setOnClickListener {
-
-            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
-                val statusBarManager =
-                    getSystemService(Context.STATUS_BAR_SERVICE) as StatusBarManager
-                /**
-                 * Important: Change the package name accordingly to your own package name!
-                 */
-                statusBarManager.requestAddTileService(
-                    ComponentName(
-                        "com.example.droidcondemoapp",
-                        "com.example.droidcondemoapp.MySampleTileService",
-                    ),
-                    "Droidcon Example App",
-                    Icon.createWithResource(this, R.drawable.ic_launcher_foreground),
-                    {},
-                    {}
-                )
-            } else {
-                Toast.makeText(
-                    this,
-                    "`requestAddTileService` can only be called in Android 13/Tiramisu.",
-                    Toast.LENGTH_SHORT,
-                ).show()
-            }
-        }
-    }
-
-    // onActivityResult() handles callbacks from the photo picker.
-    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
-        super.onActivityResult(requestCode, resultCode, data)
-        if (resultCode != Activity.RESULT_OK) {
-            // Handle error
-            return
-        }
-        // Get photo picker response for single select.
-        val currentUri: Uri = data?.data!!
-        findViewById<ImageView>(R.id.picture).setImageURI(currentUri)
     }
 
 }
