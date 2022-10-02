d02acb27e8f3071f0b260d8420a39ec1cd44dcad
Marco Zanetti <ext.marco.zanetti@deltatre.com>
2022-10-02 07:16:23 +0200

    Added Adaptive icon in order to adapt to phone themes

--- app/src/main/java/com/example/droidcondemoapp/MainActivity.kt
+++ app/src/main/java/com/example/droidcondemoapp/MainActivity.kt
@@ -2,31 +2,21 @@ package com.example.droidcondemoapp
 
 import android.Manifest
 import android.annotation.SuppressLint
-import android.app.Activity
 import android.app.LocaleManager
 import android.app.StatusBarManager
 import android.content.ComponentName
 import android.content.Context
-import android.content.Intent
 import android.graphics.drawable.Icon
-import android.net.Uri
 import android.os.Build
 import android.os.Bundle
 import android.os.LocaleList
-import android.os.ext.SdkExtensions.getExtensionVersion
-import android.provider.MediaStore
-import android.util.AttributeSet
 import android.util.Log
-import android.view.View
 import android.widget.Button
-import android.widget.ImageView
 import android.widget.TextView
 import android.widget.Toast
-import androidx.activity.result.PickVisualMediaRequest
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.annotation.RequiresApi
 import androidx.appcompat.app.AppCompatActivity
-import androidx.constraintlayout.widget.ConstraintLayout
 import java.util.*
 
 
@@ -50,10 +40,6 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-        findViewById<ConstraintLayout>(R.id.background_layout).setOnClickListener {
-            startPhotoPickerOrWhatev()
-        }
 
         findViewById<Button>(R.id.button_for_tile).setOnClickListener {
 
@@ -118,21 +104,4 @@ class MainActivity : AppCompatActivity() {
                 "Current In-App Language: $language"
         }
     }
-
-    @RequiresApi(Build.VERSION_CODES.TIRAMISU)
-    fun startPhotoPickerOrWhatev() {
-        if (isPhotoPickerAvailable()) {
-            // Include only one of the following calls to launch(), depending on the types
-            // of media that you want to allow the user to choose from.
-
-            // Launch the photo picker and allow the user to choose images and videos.
-            pickMedia.launch(PickVisualMediaRequest(ActivityResultContracts.PickVisualMedia.ImageAndVideo))
-        } else {
-            val GALLERY = 129384
-            Toast.makeText(this@MainActivity, "Missing Photo Picker!", Toast.LENGTH_SHORT).show()
-            intent = Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI)
-            startActivityForResult(intent, GALLERY)
-        }
-    }
-
 }
